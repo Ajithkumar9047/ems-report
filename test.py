c8e7eb5de7b6f000731df60ef06c92d41bd3160a
Ajithkumar9047 <ajithkumaraji9047@gmail.com>
2024-08-24 21:18:13 +0530

    first

---
+++ test.py
@@ -0,0 +1,17 @@
+import subprocess
+from Config import *
+# Replace 'your_executable.exe' with the path to your executable file
+executable_path = "C:/TVShosurRespush/LeadPush/LeadPush/bin/Debug/LeadPush.exe"
+
+def run():
+    try:
+        # Run the executable
+        subprocess.run(executable_path, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running the executable: {e}")
+        logging.error(f"Error running the executable: {e}")
+    except FileNotFoundError:
+        print(f"Executable not found at {executable_path}")
+        logging.error(f"Executable not found at {executable_path}")
+
+
